baab6b208b64aa809120d2fd38bbcf02549fcd2f
110560796CW <110560796@ea.edin.sch.uk>
2021-03-26 10:08:15 +0000

    username generator (doesn't work)

--- Main.java
+++ Main.java
@@ -1,5 +1,36 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+
+  //varaibles  
+ String [] endings = {"Ing", "end", "axe", "gex", "Goh"};
+ int numOfStudent = Keyboard.getInt("Please enter the amount of students");
+ String output = " ";
+ 
+ String ending;
+ 
+
+//loop
+ for(int index = 1; index < numOfStudent + 1; index ++){
+ String studentName = Keyboard.getText("Please Enter the first 3 letters of student number " + index + "'s name");
+ //loop to collect the names
+ while (studentName.length() != 3) {
+ Screen.display("the length of the name was invalid please try again", "ERROR");
+ studentName = Keyboard.getText("Please enter the first 3 letters of student number " + index + "'s name");
+ }
+ //adding name and endings together
+ ending = endings[N5.randomInt(4)];
+ String username = studentName + ending;
+ }
+//display
+ for(int index = 0; index < numOfSudents; index ++){
+ output = output + usernameTwo + "\n";
+ }
+
+ Screen.display(output,"username generator");
+
+ }
+
+
+
+  
 }
